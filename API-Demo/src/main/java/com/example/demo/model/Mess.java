47facf60e5d827e9361d9d1c56d3657612e44d7d
bachdz <xuanbach11696@gmail.com>
2020-12-31 18:53:40 +0100

    add mess successful

--- API-Demo/src/main/java/com/example/demo/model/Mess.java
+++ API-Demo/src/main/java/com/example/demo/model/Mess.java
@@ -2,9 +2,15 @@ package com.example.demo.model;
 
 public class Mess {
     private String mess;
+
     public Mess(String mess) {
+
         this.mess = mess;
     }
+
+    public Mess() {
+    }
+
 
     public String getMess() {
         return mess;
